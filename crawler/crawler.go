fea73d153e0556ae9bc6e404cccf2c0f2dc60578
Naithar01 <eeung025@gmail.com>
2023-04-05 01:36:21 +0900

    크롤링 코드를 분해

---
+++ crawler/crawler.go
@@ -0,0 +1,134 @@
+package crawler
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Post struct {
+	Id     string
+	Title  string
+	Writer string
+}
+
+var (
+	URL   string = "https://gall.dcinside.com/board/lists/?id=programming&page=%d"
+	mutex sync.Mutex
+)
+
+func requestCrawlerSite(page int, docChan chan *goquery.Document) error {
+	url := fmt.Sprintf(URL, page)
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		return err
+	}
+
+	docChan <- doc
+
+	return nil
+}
+
+// 페이지 * 10 페이지의 데이터를 가져오는 함수
+// page_count 1이면 1 페이지 ~ 9 페이지의 글을 가져옴
+func crawler_Pages(posts *[]Post, page_count int) {
+	docChan := make(chan *goquery.Document, 10)
+
+	go func() {
+		var StartPage, EndPage int
+		if page_count == 1 {
+			StartPage = 1
+			EndPage = 9
+		} else {
+			StartPage = (page_count - 1) * 10
+			EndPage = page_count*10 - 1
+		}
+		for page := StartPage; page <= EndPage; page++ {
+			requestCrawlerSite(page, docChan)
+		}
+		close(docChan)
+	}()
+
+	for doc := range docChan {
+		doc.Find("tr.ub-content").Each(func(i int, s *goquery.Selection) {
+			id := s.Find("td.gall_num").Text()
+			title := s.Find("td.gall_tit > a").Text()
+			writer := s.Find("td.gall_writer").AttrOr("data-nick", "ㅇㅇ")
+			post := Post{Id: id, Title: title, Writer: writer}
+			mutex.Lock()
+			*posts = append(*posts, post)
+			mutex.Unlock()
+		})
+	}
+}
+
+// 매개변수로 온 페이지의 게시물만 가져오는 함수
+// 위의 함수랑 절대 다름
+func crawler_Page(posts *[]Post, page_count int) {
+	docChan := make(chan *goquery.Document, 10)
+
+	go func() {
+		requestCrawlerSite(page_count, docChan)
+		close(docChan)
+	}()
+
+	for doc := range docChan {
+		doc.Find("tr.ub-content").Each(func(i int, s *goquery.Selection) {
+			id := s.Find("td.gall_num").Text()
+			title := s.Find("td.gall_tit > a").Text()
+			writer := s.Find("td.gall_writer").AttrOr("data-nick", "ㅇㅇ")
+			post := Post{Id: id, Title: title, Writer: writer}
+			mutex.Lock()
+			*posts = append(*posts, post)
+			mutex.Unlock()
+		})
+	}
+}
+
+func Pages() {
+	// 실행속도를 확인하기 위한
+	start := time.Now()
+
+	// 한 페이지에 게시글은 총 51개
+	var posts []Post
+
+	page_count := 1
+
+	for i := 1; i <= page_count; i++ {
+		crawler_Pages(&posts, i)
+	}
+
+	log.Println("crawler_Pages Get Data length:: ", len(posts))
+
+	log.Println(time.Now().Sub(start).Seconds(), "/s")
+}
+
+// 두 번째 행부터의 값을 반환하는 이유는
+// 공지 게시글을 선택해오기 때문임
+func Page() []Post {
+	// 실행속도를 확인하기 위한
+	start := time.Now()
+
+	// 한 페이지에 게시글은 총 51개
+	var posts []Post
+
+	page_count := 1
+
+	crawler_Page(&posts, page_count)
+
+	log.Println("crawler_Page Get Data length:: ", len(posts))
+
+	log.Println(time.Now().Sub(start).Seconds(), "/s")
+
+	return posts[2:]
+}
--- main.go
+++ main.go
@@ -1,135 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"net/http"
-	"sync"
-	"time"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/Naithar01/dc_cli_crawler/crawler"
 )
-
-type Post struct {
-	Id     string
-	Title  string
-	Writer string
-}
-
-var (
-	URL   string = "https://gall.dcinside.com/board/lists/?id=programming&page=%d"
-	mutex sync.Mutex
-)
-
-func RequestCrawlerSite(page int, docChan chan *goquery.Document) error {
-	url := fmt.Sprintf(URL, page)
-	res, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	doc, err := goquery.NewDocumentFromResponse(res)
-	if err != nil {
-		return err
-	}
-
-	docChan <- doc
-
-	return nil
-}
-
-// 페이지 * 10 페이지의 데이터를 가져오는 함수
-// page_count 1이면 1 페이지 ~ 9 페이지의 글을 가져옴
-func Crawler_Pages(posts *[]Post, page_count int) {
-	docChan := make(chan *goquery.Document, 10)
-
-	go func() {
-		var StartPage, EndPage int
-		if page_count == 1 {
-			StartPage = 1
-			EndPage = 9
-		} else {
-			StartPage = (page_count - 1) * 10
-			EndPage = page_count*10 - 1
-		}
-		for page := StartPage; page <= EndPage; page++ {
-			RequestCrawlerSite(page, docChan)
-		}
-		close(docChan)
-	}()
-
-	for doc := range docChan {
-		doc.Find("tr.ub-content").Each(func(i int, s *goquery.Selection) {
-			id := s.Find("td.gall_num").Text()
-			title := s.Find("td.gall_tit > a").Text()
-			writer := s.Find("td.gall_writer").AttrOr("data-nick", "ㅇㅇ")
-			post := Post{Id: id, Title: title, Writer: writer}
-			mutex.Lock()
-			*posts = append(*posts, post)
-			mutex.Unlock()
-		})
-	}
-}
-
-// 매개변수로 온 페이지의 게시물만 가져오는 함수
-// 위의 함수랑 절대 다름
-func Crawler_Page(posts *[]Post, page_count int) {
-	docChan := make(chan *goquery.Document, 10)
-
-	go func() {
-		RequestCrawlerSite(page_count, docChan)
-		close(docChan)
-	}()
-
-	for doc := range docChan {
-		doc.Find("tr.ub-content").Each(func(i int, s *goquery.Selection) {
-			id := s.Find("td.gall_num").Text()
-			title := s.Find("td.gall_tit > a").Text()
-			writer := s.Find("td.gall_writer").AttrOr("data-nick", "ㅇㅇ")
-			post := Post{Id: id, Title: title, Writer: writer}
-			mutex.Lock()
-			*posts = append(*posts, post)
-			mutex.Unlock()
-		})
-	}
-}
-
-func Pages() {
-	// 실행속도를 확인하기 위한
-	start := time.Now()
-
-	// 한 페이지에 게시글은 총 51개
-	var posts []Post
-
-	page_count := 2
-
-	for i := 1; i <= page_count; i++ {
-		Crawler_Pages(&posts, i)
-	}
-
-	log.Println("Crawler_Pages Get Data length:: ", len(posts))
-
-	log.Println(time.Now().Sub(start).Seconds(), "/s")
-}
-
-func Page() {
-	// 실행속도를 확인하기 위한
-	start := time.Now()
-
-	// 한 페이지에 게시글은 총 51개
-	var posts []Post
-
-	page_count := 1
-
-	Crawler_Page(&posts, page_count)
-
-	log.Println("Crawler_Page Get Data length:: ", len(posts))
-
-	log.Println(time.Now().Sub(start).Seconds(), "/s")
-}
 
 func main() {
-	Pages()
-	Page()
+	var count int
+	var reverse bool
+
+	// -n 5 -r 이런 식으로
+	// 	go build cli-app.go
+	// ./cli-app -n 5 -r
+	flag.IntVar(&count, "n", 10, "number of items to show")
+	flag.BoolVar(&reverse, "r", false, "show results in reverse order")
+
+	flag.Parse()
+
+	data := crawler.Page()
+
+	if reverse {
+		for i := len(data) - 1; i >= 0 && count > 0; i-- {
+			fmt.Println(data[i])
+			count--
+		}
+	} else {
+		for i := 0; i < len(data) && count > 0; i++ {
+			fmt.Println(data[i])
+			count--
+		}
+	}
 }
